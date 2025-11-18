ac0668d59dd6f4a4fbe5d19e27d0aa56bcd3b55c
gtapiero <giovanny.tapiero@pi.edu.co>
2025-11-18 14:21:06 -0500

    commit-MainApp .

--- src/main/java/com/example/xmlprinteroracle/MainApp.java
+++ src/main/java/com/example/xmlprinteroracle/MainApp.java
@@ -1,19 +1,27 @@
 package com.example.xmlprinteroracle;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.stage.FileChooser;
+
 import javafx.stage.Stage;
 
+import java.io.File;
 import java.io.IOException;
 
 public class MainApp extends Application {
+
+    private TextArea xmlArea;
+    private Label statusLabel;
+    private File currentFile;
+
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(MainApp.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage primaryStage) {
+
     }
+
 }
